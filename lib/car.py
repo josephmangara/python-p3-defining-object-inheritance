6e6d7fdce00d716fa1dbdf2b6467d0d2e6ad5589
josephmangara <josephmangara2@gmail.com>
2023-12-13 20:57:34 +0300

    done

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,12 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, name, wheel_size, wheel_number):
+        super().__init__(wheel_size, wheel_number)
+        self.name = name
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+mazda = Car("Mazda", 21, 4)
+print(mazda.go())
